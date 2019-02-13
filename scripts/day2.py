88ff37f077829ca4daab2126ee1972b89977cf7a
Fotone <jaemuon5582@gmail.com>
2019-02-13 19:52:08 +0900

    erqwer
    wer

--- scripts/day2.py
+++ scripts/day2.py
@@ -22,6 +22,7 @@ class simpleNet:
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -36,36 +37,29 @@ X = tf.placeholder(tf.float32, [None, 784])
 Y = tf.placeholder(tf.float32, [None, 10])
 
 # weights & bias for nn layers
-W1 = tf.Variable(tf.random_normal([784, 50]))
-b1 = tf.Variable(tf.random_normal([50]))
-L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+W1 = tf.Variable(tf.random_normal([784, 100]))
+b1 = tf.Variable(tf.random_normal([100]))
+L1 = tf.nn.sigmoid(tf.matmul(X, W1) + b1)
 
-
-W2 = tf.Variable(tf.random_normal([50, 50]))
-b2 = tf.Variable(tf.random_normal([50]))
-L2 = L1 = tf.nn.relu(tf.matmul(L1, W2) + b2)
-
-W3 = tf.Variable(tf.random_normal([50, 10]))
+W3 = tf.Variable(tf.random_normal([100, 10]))
 b3 = tf.Variable(tf.random_normal([10]))
 
 # parameters
 learning_rate = 0.001
-num_epochs = 100
-batch_size = 100
+num_epochs = 1000
+batch_size = 2000
 num_iterations = int(mnist.train.num_examples / batch_size)
 
-hypothesis = tf.matmul(L2, W3) + b3
+hypothesis = tf.matmul(L1, W3) + b3
 
 # define cost/loss & optimizer
-cost = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits_v2(
-        logits=hypothesis, labels=tf.stop_gradient(Y)
-    )
-)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=hypothesis, labels=tf.stop_gradient(Y)))
 train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 correct_prediction = tf.equal(tf.argmax(hypothesis, axis=1), tf.argmax(Y, axis=1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+train_cost_list = []
 
 # train my model
 with tf.Session() as sess:
@@ -80,6 +74,7 @@ with tf.Session() as sess:
             _, cost_val = sess.run([train, cost], feed_dict={X: batch_xs, Y: batch_ys})
             avg_cost += cost_val / num_iterations
 
+        train_cost_list.append(avg_cost)
         print(f"Epoch: {(epoch + 1):04d}, Cost: {avg_cost:.9f}")
 
     print("Learning Finished!")
@@ -106,4 +101,10 @@ with tf.Session() as sess:
         cmap="Greys",
         interpolation="nearest",
     )
+    plt.show()
+
+    x = np.arange(len(train_cost_list))
+    plt.plot(x, train_cost_list)
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
     plt.show()
